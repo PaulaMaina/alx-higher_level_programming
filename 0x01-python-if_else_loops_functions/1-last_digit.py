06e7605b99b811335e8e9321b2e63ced8044f1eb
PaulaMaina <kabura.mainap@gmail.com>
2024-01-02 12:57:04 -0800

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,6 @@ if last_digit > 5:
     string += "and is greater than 5"
 elif last_digit == 0:
     string += "and is 0"
-elif last_digit < 6 and number != 0:
+else:
     string += "and is less than 6 and not 0"
 print(string)
