3f8fa54e53df5184b23e7375c5509087241b3ce2
PaulaMaina <kabura.mainap@gmail.com>
2024-01-04 02:04:32 -0800

    Command-line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv[1:]) == 0:
+        print("{} arguments.".format(len(argv[1:])))
+    elif len(argv[1:]) == 1:
+        print("{} argument:".format(len(argv[1:])))
+    else:
+        print("{} arguments:".format(len(argv[1:])))
+    for argument in argv[1:]:
+        print("{}: {}".format((argv[1:].index(argument)) + 1, argument))
