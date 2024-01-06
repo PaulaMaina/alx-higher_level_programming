281438ece05655d6745e55b40ca86cc9982e2dda
PaulaMaina <kabura.mainap@gmail.com>
2024-01-06 05:48:22 -0800

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None:
-        return None
-
     evenList = my_list[:]
 
     for num in evenList:
--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -2,9 +2,17 @@
 divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
 
 my_list = [0, 1, 2, 3, 4, 5, 6]
+my_list2 = []
 list_result = divisible_by_2(my_list)
+list_result2 = divisible_by_2(my_list2)
 
 i = 0
 while i < len(list_result):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
     i += 1
+
+print("Length: {}".format(len(list_result)))
+for i in list_result:
+    print(i, end=", ")
+
+print(list_result2)
