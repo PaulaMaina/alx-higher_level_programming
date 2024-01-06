5b95345d8c9b4e32c41319cdef04be73c0d74f0e
PaulaMaina <kabura.mainap@gmail.com>
2024-01-06 03:31:48 -0800

    Add 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = list(tuple_a)
+    tuple_b = list(tuple_b)
+    if len(tuple_a) == 0:
+        tuple_a.append(0)
+        tuple_a.append(0)
+    if len(tuple_a) == 1:
+        tuple_a.append(0)
+    if len(tuple_b) == 0:
+        tuple_b.append(0)
+        tuple_b.append(0)
+    if len(tuple_b) == 1:
+        tuple_b.append(0)
+
+    total = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return total
