27544b037ab00b2845009a18aa1c735f7b81811d
PaulaMaina <kabura.mainap@gmail.com>
2024-01-10 09:30:43 -0800

    Task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    dict_keys =  sorted(a_dictionary)
+    dict_new = {}
+    for k in dict_keys:
+        dict_new[k] = a_dictionary[k]
+    for k in dict_new:
+        print("{} : {}".format(k, dict_new[k]))
