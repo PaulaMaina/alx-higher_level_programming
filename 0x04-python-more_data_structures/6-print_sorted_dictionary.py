dab02991d5c5628728ff8099a435981ee0bcb3ad
PaulaMaina <kabura.mainap@gmail.com>
2024-01-10 09:34:14 -0800

    Task 6 debug

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_keys =  sorted(a_dictionary)
+    dict_keys = sorted(a_dictionary)
     dict_new = {}
     for k in dict_keys:
         dict_new[k] = a_dictionary[k]
