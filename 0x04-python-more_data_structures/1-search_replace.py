e8a72ece276983330274163204f8d7fefe07cf0c
PaulaMaina <kabura.mainap@gmail.com>
2024-01-10 09:13:24 -0800

    Taks 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replace_list = []
+
+    for i in my_list:
+        if i is search:
+            replace_list.append(replace)
+        else:
+            replace_list.append(i)
+    return replace_list
