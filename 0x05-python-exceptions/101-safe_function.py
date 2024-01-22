0921bea49264e3f48a80e5b734cfcddd15193dea
PaulaMaina <kabura.mainap@gmail.com>
2024-01-22 03:20:07 -0800

    Execute function sucessfully

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ZeroDivisionError, TypeError, IndexError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
