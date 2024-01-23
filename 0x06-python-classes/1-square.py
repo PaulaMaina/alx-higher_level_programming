3d561372bb471ad80953f2552b353bbc2b36f8b4
PaulaMaina <kabura.mainap@gmail.com>
2024-01-23 05:46:29 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class definition with a private attribute size"""
+
+
+class Square():
+    """Definition of 'Square'"""
+    def __init__(self, size=0):
+        """Initializes an object of square with a private instance attribute 'size'
+
+            Args:
+                size: size of a square
+        """
+        self.__size = size
