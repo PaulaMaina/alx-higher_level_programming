d85afcac816709ef2dcb66c13c60394fffe47fb9
PaulaMaina <kabura.mainap@gmail.com>
2024-02-05 13:48:07 +0300

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass of Rectangle"""
+    def __init__(se;f, size):
+        """Initiates the width and height to size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
