f28c296ad4f92d09cb41a19d14a3f59b5cc41ba1
PaulaMaina <kabura.mainap@gmail.com>
2024-02-05 12:52:13 +0300

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""MyList subclass"""
+
+
+class MyList:
+    """Subclass of List class"""
+
+    def print_sorted(self):
+        """Prints a list sorted in ascending order"""
+        print(sorted(self))
