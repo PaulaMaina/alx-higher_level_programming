292a995f77573dc3c9179bd26450d8b08be52486
PaulaMaina <kabura.mainap@gmail.com>
2024-02-06 11:08:55 +0300

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#1/usr/bin/python3
+"""Save to JSON module"""
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using a JSON representation
+
+        Args:
+            my_obj: the object
+            filename: the name of the file
+    """
+    with open(filename, "w", encoding="uft-8") as f:
+        f.write(json.dumps(my_obj))
