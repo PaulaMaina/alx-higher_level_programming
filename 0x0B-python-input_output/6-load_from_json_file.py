db45794bef58990b5debfe51e80bd3125a0e5edc
PaulaMaina <kabura.mainap@gmail.com>
2024-02-06 11:14:39 +0300

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Load from JSON file"""
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file
+
+        Args:
+            filename: The name of the file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f.read())
