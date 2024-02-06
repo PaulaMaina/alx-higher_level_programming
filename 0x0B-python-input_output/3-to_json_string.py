f469434c117b6ee2fea6902d9a15f731ecd85d2c
PaulaMaina <kabura.mainap@gmail.com>
2024-02-06 10:58:14 +0300

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""JSON string module"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON string of an object
+
+        Args:
+            my_obj: object
+        Returns:
+            The JSON string of the object
+    """
+    return json.dumps(my_obj)
