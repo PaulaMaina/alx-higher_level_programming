cc89718edf0234926684d59c8164fe0c956dfa87
PaulaMaina <kabura.mainap@gmail.com>
2024-02-06 19:02:36 +0300

    Task 7 debug

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Adds command-line arguments to a file"""
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__(8'-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__(6'-load_from_json_file').load_from_json_file
 
 try:
     arguments = load_from_json_file("add_item.json")
