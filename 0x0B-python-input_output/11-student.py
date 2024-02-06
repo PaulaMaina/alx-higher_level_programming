f9c9b61953e01833b83670ad901f036197805d0e
PaulaMaina <kabura.mainap@gmail.com>
2024-02-06 19:06:02 +0300

    Task 11 debug

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,7 +10,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
+    def to_json(self, attrs=None):
         """Retrieves the dictionary representation of an instance"""
         my_dict = {}
         if isinstance(attrs, list):
