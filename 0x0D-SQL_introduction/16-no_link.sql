497b314ccbc03a71fe8ea2c8c8a073bfeea78994
PaulaMaina <kabura.mainap@gmail.com>
2024-03-12 16:42:05 +0300

    List records with score and name, sorted by the score

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists all records of second_table
+-- Rows should include the name
+-- Results should be display the score followed by the name
+-- Records should be listed descending score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
