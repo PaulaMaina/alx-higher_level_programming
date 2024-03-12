5db69e4825d306aa126e1327da2854ebc4adb460
PaulaMaina <kabura.mainap@gmail.com>
2024-03-12 17:00:25 +0300

    Display average temperatures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Displays the average temperatures by city
+-- The list is ordered by temperature in descending order
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
