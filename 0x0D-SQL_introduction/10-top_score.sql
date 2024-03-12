6e413f295a04bace3b05ec3e7002fd9af0a0a7c2
PaulaMaina <kabura.mainap@gmail.com>
2024-03-12 15:40:18 +0300

    List records of second_table by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records of second_table
+-- Results should display both score and the name(in this order)
+-- Records should be ordered by score
+SELECT score, name
+FROM second_table
+ORDER BY score, name;
