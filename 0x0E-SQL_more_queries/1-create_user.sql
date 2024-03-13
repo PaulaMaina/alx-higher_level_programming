2146f95252662cc5b797a0f82305a33247de80ee
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 13:09:36 +0300

    Debugging

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- Creates a new user user_0d_1
--- Sets the user's password to 'user_0d_1_pwd'
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED WITH `user_0d_1_pwd`;
--- Grant all privileges to the new user
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+-- Creates a new user user_0d_1 and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
