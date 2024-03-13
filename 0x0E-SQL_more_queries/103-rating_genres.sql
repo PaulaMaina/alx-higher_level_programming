3a90c85a2befaef68bebdba6d904b14418caf91c
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 17:48:22 +0300

    Rating genres

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all shows by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genre.show_id = tv_show_ratings.show_id
+GROUP BY tv_genre.name
+ORDER BY rating DESC;
