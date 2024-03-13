3e6ed985ed002e6711df257baeaa1a57e45fe9b0
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 17:44:36 +0300

    Rating shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings
-ON tV_shows.id = tv_show_ratings.show_id
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
