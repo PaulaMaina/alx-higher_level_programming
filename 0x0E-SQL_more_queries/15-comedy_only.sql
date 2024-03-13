2ec7f4bb57559bdfbeba837b58e7e22a65ea35a1
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 16:54:19 +0300

    Comedy shows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@ FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-ON tv_genre.id = tv_show_genres.genre_id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres = 'Comedy'
 ORDER BY tv_shows.title;
