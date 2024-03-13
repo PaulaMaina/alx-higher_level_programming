9fe17677fab9522021c017488b42ff1add1b32e3
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 16:16:48 +0300

    Tv shows by genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_shows_genre.genre_id
 FROM tv_shows
 INNER JOIN tv_shows_genre
 ON tv_shows.id = tv_shows_genre.show_id
-ORDER BY tv_shows.title, tv_shows_genre.id;
+ORDER BY tv_shows.title, tv_shows_genre.genre_id;
