bc6c51abb0681977a7d96888e4becab062b9d541
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:48:09 +0300

    Create hbtn_0d_usa and cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ USE `hbtn_0d_usa`;
 -- Creates the table cities if it does not exist
 CREATE TABLE IF NOT EXISTS cities (
 	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id int FOREIGN KEY(state_id) REFERENCES states(id),
+	state_id int NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
 	name varchar(256)
 );
