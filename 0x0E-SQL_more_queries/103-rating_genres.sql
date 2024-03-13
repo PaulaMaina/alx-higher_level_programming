cd4f9d0a1e45a267fcee48de239df62a2ae09a6e
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 17:50:20 +0300

    Rating genres

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,6 +4,6 @@ FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings
-ON tv_show_genre.show_id = tv_show_ratings.show_id
-GROUP BY tv_genre.name
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
