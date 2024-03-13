8487c144dfedfa68ec47678a9fc14918a325ef2e
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:43:36 +0300

    Create hbtn_0d_usa and states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- Creates database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- Uses the created database
+USE `hbtn_0d_usa`;
 -- Creates the table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
-	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY_KEY,
+	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name varchar(256)
 );
