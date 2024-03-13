9757f82f92050c9b8097a6a23383e11f975beee9
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:24:22 +0300

    Debug

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create a database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 -- Create a user user_0d_2 and set password to user_0d_2_pwd
-CREATE  USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WITH 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WITH 'user_0d_2_pwd';
 -- Grant SELECT privilege to the user
 GRANT SELECT ON `hbtn_0d_2`.* TO user_0d_2@localhost;
