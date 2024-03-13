1f40ad27d63128c97f041f7214ce2162053e4502
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 16:18:00 +0300

    Tv shows by genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all shows that have at least one genre linked
-SELECT tv_shows.title, tv_shows_genre.genre_id
+SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_shows_genre
-ON tv_shows.id = tv_shows_genre.show_id
-ORDER BY tv_shows.title, tv_shows_genre.genre_id;
+INNER JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
