3d60a0a0d255739e6de4460855e8540f2ec2519c
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:23:22 +0300

    Adding comments for each query

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates a new user user_0d_1 and grant all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to new user
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
