bf8641550f67b831449b597fa1ce79978b267730
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:50:20 +0300

    Create hbtn_0d_usa and cities; debug

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS cities (
 	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id int NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
-	name varchar(256)
+	name varchar(256) NOT NULL
 );
