47bfca8cc50dc784ee664dafe11526b7ecc9bd6c
PaulaMaina <kabura.mainap@gmail.com>
2024-03-13 15:51:06 +0300

    Debug

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,5 +5,5 @@ USE `hbtn_0d_usa`;
 -- Creates the table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
 	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name varchar(256)
+	name varchar(256) NOT NULL
 );
