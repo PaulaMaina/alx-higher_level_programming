0f1a739e9ee33fb87b7c971ccc1f96280a6381dd
PaulaMaina <kabura.mainap@gmail.com>
2024-04-08 06:38:35 -0700

    Loop to languages

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,8 @@
 
 const { argv } = require('node:process');
 let myInt = parseInt(argv[2]);
-if (typeof myInt === NaN) {
+console.log(myInt);
+if (myInt === NaN) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${argv[2]}`);
