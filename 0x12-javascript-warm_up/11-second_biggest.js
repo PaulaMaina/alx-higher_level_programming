93cd851a8a7ae47384bad91b109b9cdba9ef0cec
PaulaMaina <kabura.mainap@gmail.com>
2024-04-08 08:56:26 -0700

    Second biggest debug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,9 @@
 
 const nums = process.argv.slice(2);
 
-if (nums.length <= 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(nums.map(num => parseInt(num)).sort((i, j) => j - i)[1]);
+  const nums = process.argv.slice(2).map(Number);
+  console.log(nums.sort((i, j) => j - i)[1]);
 }
