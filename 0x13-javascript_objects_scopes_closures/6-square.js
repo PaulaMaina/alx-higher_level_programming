e5d33a090446842dcf817a1ed69d872b7835d73c
PaulaMaina <kabura.mainap@gmail.com>
2024-04-09 03:24:56 -0700

    Sqaure

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const Square5 = require('./5-square.js');
+module.exports = class Square extends Square5 {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let side = '';
+      for (let j = 0; j < this.width; j++) {
+        side += c;
+      }
+      console.log(side);
+    }
+  }
+};
