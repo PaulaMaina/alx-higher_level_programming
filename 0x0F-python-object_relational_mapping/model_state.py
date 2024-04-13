ae0e707266cc7350b96c622352695b04166bf46f
PaulaMaina <kabura.mainap@gmail.com>
2024-04-13 15:19:14 +0300

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Contains the class definition of a State and a Base instance"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, Column, Integer, String
+
+metaData = MetaData()
+Base = declarative_base(metadata=metaData)
+
+
+class State(Base):
+    """Definition of the State class with id and name attributes"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(Integer(128), nullable=False)
