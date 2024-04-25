4e709b35ba7d6230f9749f59112611a3c8bd55d3
PaulaMaina <kabura.mainap@gmail.com>
2024-04-25 11:09:32 -0700

    Task 4 debug

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to the URL passed as an argument and dispays
 # the response
-curl -sH "X-school-User-Id: 98" "$1" 
+curl -sH "X-School-User-Id: 98" "$1" 
