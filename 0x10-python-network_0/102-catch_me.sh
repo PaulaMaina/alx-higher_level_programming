30a6623b187f449f20e5218d99931beb5566bcac
PaulaMaina <kabura.mainap@gmail.com>
2024-04-25 11:56:02 -0700

    Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me that returns You got me!
-curl -s -o /dev/null -w "You got me!" 127.0.0.1:5000/catch_me
+curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
