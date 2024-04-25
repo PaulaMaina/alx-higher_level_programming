3d2f59d3796fb8d392f677363684ab671bfca2b3
PaulaMaina <kabura.mainap@gmail.com>
2024-04-25 11:22:30 -0700

    Peak debug

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to the URL passed and displays the body
-curl -s -H "Content-Type: application/json" -d @ "$2" "$1" "http://localhost:5000"
+curl -s -H "Content-Type: application/json" -d @"$2" "$1" "http://localhost:5000"
