31f50f0da37d35cbd6c83826e4e82d3c0a76b766
PaulaMaina <kabura.mainap@gmail.com>
2024-04-25 10:59:37 -0700

    Debug

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me that causes the server
 # to respond with a message containing You got me!
-curl -s -o /dev/null -w "You got me!" 127.0.0.1:5000/catch_me
+curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
