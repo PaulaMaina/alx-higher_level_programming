c36095080ced274d01bac73edf0ce9e69f1b9c05
PaulaMaina <kabura..mainap@gmail.com>
2024-04-26 15:39:15 +0300

    Search user

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ def search_user(letter: str):
     letter_data = {"q": letter}
     url = "http://0.0.0.0:5000/search_user"
     req = requests.post(url, data=letter_data)
-    
+
     try:
         req_dict = req.json()
         if req_dict:
