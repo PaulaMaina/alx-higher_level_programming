34b67af476e53f2bf7318343f9b95f81e443a98a
PaulaMaina <kabura..mainap@gmail.com>
2024-04-26 13:02:26 +0300

    Task 0 debug

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,6 @@ from urllib.error import URLError
 
 
 def fetch_body():
-    """Function to fetch the URL"""
     try:
         req = Request("https://alx-intranet.hbtn.io/status")
         with urlopen(req) as response:
