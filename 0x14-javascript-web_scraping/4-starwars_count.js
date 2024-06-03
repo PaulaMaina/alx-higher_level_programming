13e7972c56f5a47f7ba621d67202741a308c3756
PaulaMaina <kabura.mainap@gmail.com>
2024-06-03 06:48:10 -0700

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2];
+let num = 0;
+
+req.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    content.results.forEach((film) => {
+      content.characters.forEach((character) => {
+        if (character.includes(18)) {
+          num += 1;
+        }
+      });
+    });
+    console.log(num);
+  }
+});
