c88456e920d1aed80171e487997bfdebb7aec140
PaulaMaina <kabura.mainap@gmail.com>
2024-06-03 06:24:44 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const file_sys = require('fs');
-file_sys.readFile(process.argv[2], 'utf-8',
-	function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
